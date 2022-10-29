fe85191d5708bbb6fe02cbe3b0006b1b6c70f152
sweetmangoes <alphonsechristian@gmail.com>
2022-10-29 17:02:16 -0400

    added /url/new route

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req,res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   console.log()
